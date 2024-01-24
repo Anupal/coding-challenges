85968cd251b84d5f646cd896235593824290f7d7
Anupal Mishra <anupal.pm@gmail.com>
2024-01-24 21:04:14 +0000

    add support for flags and format the output

--- ccwc/ccwc.go
+++ ccwc/ccwc.go
@@ -5,72 +5,142 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
+	"strconv"
 	"strings"
 )
 
-func main() {
-	fmt.Println("Command line args")
-	cmdlineArgs := os.Args
-	for index, arg := range cmdlineArgs {
-		fmt.Println(index, arg)
-	}
-
-	flagBytes, flagChars, flagLines, flatWords, fileList := captureFlags(cmdlineArgs)
-
-	// If no flags are passed
-	if !(flagBytes && flagChars && flagLines && flatWords) {
-		flagBytes = true
-		flagLines = true
-		flatWords = true
-	}
-
-	// loop over fileName list
-	for _, fileName := range fileList {
-		filePtr, err := os.Open(fileName)
-		if err != nil {
-			log.Fatalf("File '%s' not found!", fileName)
-		}
-		fileData := make([]byte, 100)
-		byteCount, err := filePtr.Read(fileData)
-		if err != nil {
-			log.Fatalf("File '%s' not readable", fileName)
-		}
-		fileData = bytes.TrimRight(fileData, "\x00")
-
-		fileDataString := string(fileData)
-		charCount := len(fileDataString)
-
-		lines := strings.Split(fileDataString, "\n")
-		lineCount := len(lines) - 1
-
-		wordCount := 0
-		for _, line := range lines {
-			wordCount += len(strings.Fields(line))
-		}
-
-		fmt.Printf("Byte count for file '%s': %d\n", fileName, byteCount)
-		fmt.Printf("Line count for file '%s': %d\n", fileName, lineCount)
-		fmt.Printf("Word count for file '%s': %d\n", fileName, wordCount)
-		fmt.Printf("Char count for file '%s': %d\n", fileName, charCount)
-	}
-
+type fileData struct {
+	name  string
+	bytes int
+	chars int
+	lines int
+	words int
 }
 
-func captureFlags(args []string) (bytes bool, chars bool, lines bool, words bool, fileList []string) {
-	fileList = make([]string, 0, 10)
+func maxWidth(flags map[string]bool, fileDataList []fileData) int {
+	maximum := 0
+
+	for _, file := range fileDataList {
+		includedValues := make([]int, 0, 4)
+		if flags["lines"] {
+			includedValues = append(includedValues, len(strconv.Itoa(file.lines)))
+		}
+		if flags["words"] {
+			includedValues = append(includedValues, len(strconv.Itoa(file.words)))
+		}
+		if flags["bytes"] {
+			includedValues = append(includedValues, len(strconv.Itoa(file.bytes)))
+		}
+		if flags["chars"] {
+			includedValues = append(includedValues, len(strconv.Itoa(file.chars)))
+		}
+		for _, value := range includedValues {
+			if value > maximum {
+				maximum = value
+			}
+		}
+	}
+
+	return maximum
+}
+
+func (file *fileData) parseFile(filePath string) {
+	_, file.name = path.Split(filePath)
+
+	filePtr, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("File '%s' not found!", file.name)
+	}
+
+	fileData := make([]byte, 100)
+	file.bytes, err = filePtr.Read(fileData)
+	if err != nil {
+		log.Fatalf("File '%s' not readable", file.name)
+	}
+	fileData = bytes.TrimRight(fileData, "\x00")
+
+	fileDataString := string(fileData)
+	file.chars = len(fileDataString)
+
+	lines := strings.Split(fileDataString, "\n")
+	file.lines = len(lines) - 1
+
+	file.words = 0
+	for _, line := range lines {
+		file.words += len(strings.Fields(line))
+	}
+}
+
+func (file *fileData) displayRow(flags map[string]bool, maximumWidth int) {
+	if flags["lines"] {
+		fmt.Printf("%*d ", maximumWidth, file.lines)
+	}
+	if flags["words"] {
+		fmt.Printf("%*d ", maximumWidth, file.words)
+	}
+	if flags["bytes"] {
+		fmt.Printf("%*d ", maximumWidth, file.bytes)
+	}
+	if flags["chars"] {
+		fmt.Printf("%*d ", maximumWidth, file.chars)
+	}
+	fmt.Printf("%s\n", file.name)
+}
+
+func main() {
+	cmdlineArgs := os.Args
+
+	flags, fileList := captureFlags(cmdlineArgs)
+
+	// If no flags are passed
+	ifFlagsPassed := false
+	for _, flag := range flags {
+		ifFlagsPassed = ifFlagsPassed || flag
+	}
+
+	if !ifFlagsPassed {
+		flags["bytes"] = true
+		flags["lines"] = true
+		flags["words"] = true
+	}
+
+	fileDataList := make([]fileData, len(fileList))
+	// loop over file list
+	for i, filePath := range fileList {
+		fileDataList[i].parseFile(filePath)
+	}
+
+	maximumWidth := maxWidth(flags, fileDataList)
+
+	for _, file := range fileDataList {
+		file.displayRow(flags, maximumWidth)
+	}
+}
+
+// bytes bool, chars bool, lines bool, words bool
+func captureFlags(args []string) (map[string]bool, []string) {
+	flags := make(map[string]bool)
+	fileList := make([]string, 0, 10)
+
+	flags["bytes"] = false
+	flags["chars"] = false
+	flags["lines"] = false
+	flags["words"] = false
+
 	for _, arg := range args[1:] {
 		switch arg {
 		case "-c", "--bytes":
-			bytes = true
+			flags["bytes"] = true
 		case "-m", "--chars":
-			chars = true
+			flags["chars"] = true
 		case "-l", "--lines":
-			lines = true
+			flags["lines"] = true
 		case "-w", "--words":
-			words = true
+			flags["words"] = true
 		default:
 			fileList = append(fileList, arg)
 		}
 	}
-	return
+	return flags, fileList
 }
